e8a1e20ba40f895e9ed51d33dbdbaab8dffe1942
Tasos Laskos <tasos.laskos@gmail.com>
2015-01-09 04:51:24 +0200

    Added spec/support/helpers/paths.rb

---
+++ spec/support/helpers/paths.rb
@@ -0,0 +1,19 @@
+def root_path
+    File.expand_path( File.dirname( __FILE__ ) + '/../../..' )
+end
+
+def spec_path
+    "#{root_path}/spec"
+end
+
+def support_path
+    "#{spec_path}/support"
+end
+
+def fixtures_path
+    "#{support_path}/fixtures"
+end
+
+def fixture_path_for( file )
+    "#{fixtures_path}/#{file}"
+end
